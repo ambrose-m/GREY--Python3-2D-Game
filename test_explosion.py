6d9b75ca42bcc00a275a470eddc2b9eb2055f3ec
Madeline Ambrose <ambrose907@gmail.com>
2021-05-26 16:36:06 -0700

    Replaced extra explosion class instances with a setUp method.

--- test_explosion.py
+++ test_explosion.py
@@ -1,92 +1,89 @@
 import unittest
 import coverage
 import pygame
-import ExplosionClass
+import explosion_class
 
 class ExplosionTest(unittest.TestCase):
+    def setUp(self):
+        self.explosion_sm = explosion_class.Explosion((200, 200), 'sm') 
+        self.explosion_lg = explosion_class.Explosion((210, 210), 'lg') 
+        self.explosion_xlg = explosion_class.Explosion((220, 220), 'Xlg')  
+
     #__init__(self, center, size) unit tests:
-    def test_init_size(self):   
-        exp1 = ExplosionClass.ExplosionClass((205, 220), 'sm')   
-        self.assertEqual(exp1.size, 'sm')
-        exp2 = ExplosionClass.ExplosionClass((205, 220), 'lg')     
-        self.assertEqual(exp2.size, 'lg')
-        exp3 = ExplosionClass.ExplosionClass((205, 220), 'Xlg')    
-        self.assertEqual(exp3.size, 'Xlg')
+    def test_init_size(self):    
+        self.assertEqual(self.explosion_sm.size, 'sm')  
+        self.assertEqual(self.explosion_lg.size, 'lg')
+        self.assertEqual(self.explosion_xlg.size, 'Xlg')
 
     def test_init_image(self):
-        exp1 = ExplosionClass.ExplosionClass((200, 120), 'sm') 
-        self.assertEqual(exp1.image, ExplosionClass.explosion_anim['sm'][0])
-        exp2 = ExplosionClass.ExplosionClass((205, 220), 'lg')  
-        self.assertEqual(exp2.image, ExplosionClass.explosion_anim['lg'][0])
-        exp3 = ExplosionClass.ExplosionClass((205, 220), 'Xlg')   
-        self.assertEqual(exp3.image, ExplosionClass.explosion_anim['Xlg'][0])
+        self.assertEqual(self.explosion_sm.image, explosion_class.explosion_anim['sm'][0])
+        self.assertEqual(self.explosion_lg.image, explosion_class.explosion_anim['lg'][0])
+        self.assertEqual(self.explosion_xlg.image, explosion_class.explosion_anim['Xlg'][0])
 
     def test_init_rect(self):
-        exp1 = ExplosionClass.ExplosionClass((205, 220), 'sm') 
-        self.assertEqual(exp1.rect, (189, 204, 32, 32))
-        exp2 = ExplosionClass.ExplosionClass((205, 220), 'lg')  
-        self.assertEqual(exp2.rect, (158, 173, 95, 95))
-        exp3 = ExplosionClass.ExplosionClass((205, 220), 'Xlg')   
-        self.assertEqual(exp3.rect, (100, 115, 210,210))
+        self.assertEqual(self.explosion_sm.rect, (184, 184, 32, 32))
+        self.assertEqual(self.explosion_lg.rect, (163, 163, 95, 95))
+        self.assertEqual(self.explosion_xlg.rect, (115, 115, 210,210))
 
     def test_init_rectCenter(self):
-        exp1 = ExplosionClass.ExplosionClass((205, 220), 'sm') 
-        self.assertEqual(exp1.rect.center, (205, 220))
-        exp2 = ExplosionClass.ExplosionClass((200, 200), 'lg')  
-        self.assertEqual(exp2.rect.center, (200, 200))
-        exp3 = ExplosionClass.ExplosionClass((195, 230), 'Xlg')   
-        self.assertEqual(exp3.rect.center, (195, 230))
+        self.assertEqual(self.explosion_sm.rect.center, (200, 200)) 
+        self.assertEqual(self.explosion_lg.rect.center, (210, 210))  
+        self.assertEqual(self.explosion_xlg.rect.center, (220, 220))
         
 
     def test_init_frame(self):
-        exp = ExplosionClass.ExplosionClass((205, 220), 'lg')  
-        self.assertEqual(exp.frame, 0)
+        self.assertEqual(self.explosion_sm.frame, 0)
+        self.assertEqual(self.explosion_lg.frame, 0)
+        self.assertEqual(self.explosion_xlg.frame, 0)
     
     def test_init_lastUpdate(self):
-        exp = ExplosionClass.ExplosionClass((205, 220), 'Xlg') 
-        self.assertEqual(exp.last_update, 0) 
+        self.assertEqual(self.explosion_sm.last_update, 0) 
+        self.assertEqual(self.explosion_lg.last_update, 0) 
+        self.assertEqual(self.explosion_xlg.last_update, 0) 
     
-    def test_init_framerate(self):
-        exp = ExplosionClass.ExplosionClass((205, 220), 'lg')  
-        self.assertEqual(exp.frame_rate, 50)
+    def test_init_framerate(self): 
+        self.assertEqual(self.explosion_sm.frame_rate, 50)
+        self.assertEqual(self.explosion_lg.frame_rate, 50)
+        self.assertEqual(self.explosion_xlg.frame_rate, 50)
 
     # update(self) unit tests:
     def test_update_lastUpdate(self):
-        exp = ExplosionClass.ExplosionClass((205, 220), 'lg') 
-        self.assertEqual(exp.last_update, 0) 
+        self.assertEqual(self.explosion_sm.last_update, 0) 
         now = 15000
-        exp.update(now)
-        self.assertEqual(exp.last_update, now)
+        self.explosion_sm.update(now)
+        self.assertEqual(self.explosion_sm.last_update, now)
 
     def test_update_frame(self):
-        exp = ExplosionClass.ExplosionClass((205, 220), 'sm') 
-        self.assertEqual(exp.frame, 0)
-        exp.update(15000)
-        self.assertEqual(exp.frame, 1)
-        exp.update(20000)
-        self.assertEqual(exp.frame, 2)
+        self.assertEqual(self.explosion_sm.frame, 0)
+        self.explosion_sm.update(15000)
+        self.assertEqual(self.explosion_sm.frame, 1)
+        self.explosion_sm.update(20000)
+        self.assertEqual(self.explosion_sm.frame, 2)
     
     def test_update_rect(self):
-        exp1 = ExplosionClass.ExplosionClass((205, 220), 'sm') 
-        exp1.update(20000)
-        self.assertEqual(exp1.rect, (189, 204, 32, 32))
-        exp2 = ExplosionClass.ExplosionClass((205, 220), 'lg')  
-        exp2.update(20000)
-        self.assertEqual(exp2.rect, (158, 173, 95, 95))
-        exp3 = ExplosionClass.ExplosionClass((205, 220), 'Xlg')  
-        exp3.update(20000) 
-        self.assertEqual(exp3.rect, (100, 115, 210,210))
+        self.explosion_sm.update(20000)
+        self.assertEqual(self.explosion_sm.rect, (184, 184, 32, 32)) 
+        self.explosion_lg.update(20000)
+        self.assertEqual(self.explosion_lg.rect, (163, 163, 95, 95))
+        self.explosion_xlg.update(20000) 
+        self.assertEqual(self.explosion_xlg.rect, (115, 115, 210,210))
 
     def test_update_rectCenter(self):
-        exp = ExplosionClass.ExplosionClass((195, 180), 'sm') 
-        exp.update(20000)
-        self.assertEqual(exp.rect.center, (195, 180))
+        self.explosion_sm.update(20000)
+        self.assertEqual(self.explosion_sm.rect.center, (200, 200))
     
     def test_update_image(self):
-        exp = ExplosionClass.ExplosionClass((205, 220), 'sm') 
-        self.assertEqual(exp.image, ExplosionClass.explosion_anim['sm'][0])
-        exp.update(15000)
-        self.assertEqual(exp.image, ExplosionClass.explosion_anim['sm'][1])
+        self.assertEqual(self.explosion_sm.image, explosion_class.explosion_anim['sm'][0])
+        self.explosion_sm.update(15000)
+        self.assertEqual(self.explosion_sm.image, explosion_class.explosion_anim['sm'][1])
+    
+    #TO DO: Boundary Tests 
+    #center boundaries. not exactly sure what they are but maybe lt 0 and gt ?
+    #explosion_anim[self.size][]. size lt 0 and gt ?. second[] index less than 0 and greater than 9
+    #self.frame in update(). it's the second [] in exp_anim
+
+    #TO DO: Invalid Value Tests
+    # strings and doubles for the center, size, and now variables
              
 if __name__ == '__main__':
     unittest.main()
